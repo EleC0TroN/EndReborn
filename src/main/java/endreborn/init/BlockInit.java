d8d2286af1b54fc7c09e52841eb53a187d1242b1
EleC0TroN <35199902+EleC0TroN@users.noreply.github.com>
2018-01-22 15:15:53 +0300

    Update BlockInit.java

--- src/main/java/endreborn/init/BlockInit.java
+++ src/main/java/endreborn/init/BlockInit.java
@@ -3,13 +3,18 @@ package endreborn.init;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.block.Block;
 import endreborn.objects.blocks.BlockBase;
+import endreborn.objects.blocks.LampBase;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class BlockInit 
 {
-  public static final List<Block> BLOCKS = new ArrayList<Block>();
-  
+	public static final List<Block> BLOCKS = new ArrayList<Block>();
+	
 	public static final Block BLOCK_ENDORIUM = new BlockBase("block_endorium", Material.IRON);
+	public static final Block END_STONE_SMOOTH = new BlockBase("block_end_stone_smooth", Material.ROCK);
+	public static final Block END_STONE_PILLAR = new BlockBase("block_end_stone_pillar", Material.ROCK);
+	public static final Block PURPUR_LAMP = new LampBase("block_purpur_lamp", Material.ROCK);
+
 }
