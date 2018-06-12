b42768e8309d73d4db63b138ce7bfe3bb693852c
EleC0TroN <35199902+EleC0TroN@users.noreply.github.com>
2018-06-12 18:11:47 +0300

    Update RebornofEnd.java

--- src/main/java/endreborn/mod/RebornofEnd.java
+++ src/main/java/endreborn/mod/RebornofEnd.java
@@ -3,19 +3,25 @@ package endreborn.mod;
 import endreborn.proxy.CommonProxy;
 import endreborn.tabs.EndReborn;
 import endreborn.util.Reference;
+import endreborn.util.handlers.RegistryHandler;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 public class RebornofEnd 
 {
 	public static final CreativeTabs endertab = new EndReborn("endertab");
+	
+    public static boolean activateEndGeneration;
+    public static boolean activateVanillaEndOres;
+	
+	@Instance(Reference.MODID)
+	public static RebornofEnd mod;
 	
 	@Instance
 	public static RebornofEnd instance;
@@ -26,18 +32,13 @@ public class RebornofEnd
     @EventHandler
     public static void preInit(FMLPreInitializationEvent event)
     {
-    
+		RegistryHandler.preInitRegistries();
     }
 
     @EventHandler
     public static void init(FMLInitializationEvent event)
     {
-    
-    }
 
-    @EventHandler
-    public static void postInit(FMLPostInitializationEvent event)
-    {
-   
     }
 }
+
