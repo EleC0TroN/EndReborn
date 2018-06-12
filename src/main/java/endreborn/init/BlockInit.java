93df1ac4077aa52da4ff1e62bf5ba5500150f189
EleC0TroN <35199902+EleC0TroN@users.noreply.github.com>
2018-06-12 18:09:31 +0300

    Update BlockInit.java

--- src/main/java/endreborn/init/BlockInit.java
+++ src/main/java/endreborn/init/BlockInit.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import endreborn.objects.blocks.BlockBase;
+import endreborn.objects.blocks.BlockEssenceOre;
+import endreborn.objects.blocks.BlockPhantom;
+import endreborn.objects.blocks.DragonBush;
 import endreborn.objects.blocks.EnderCropBase;
 import endreborn.objects.blocks.LampBase;
 import net.minecraft.block.Block;
@@ -18,5 +21,7 @@ public class BlockInit
 	public static final Block END_STONE_PILLAR = new BlockBase("block_end_stone_pillar", Material.ROCK);
 	public static final Block PURPUR_LAMP = new LampBase("block_purpur_lamp", Material.ROCK);
 	public static final Block ENDER_FLOWER = new EnderCropBase("crop_ender_flower");
-
+	public static final Block DRAGON_BUSH = new DragonBush("crop_dragonite");
+	public static final Block ESSENCE_ORE = new BlockEssenceOre("block_essence_ore", Material.ROCK);
+	public static final Block PHANTOM_BLOCK = new BlockPhantom("block_phantom", Material.BARRIER, false);
 }
