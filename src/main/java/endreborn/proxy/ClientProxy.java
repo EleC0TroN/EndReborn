ad3d05cfd4b40e0644cfe075b78dbdf04800a94c
EleC0TroN <35199902+EleC0TroN@users.noreply.github.com>
2018-06-12 18:15:57 +0300

    Add files via upload

--- src/main/java/endreborn/proxy/ClientProxy.java
+++ src/main/java/endreborn/proxy/ClientProxy.java
@@ -1,8 +1,11 @@
 package endreborn.proxy;
 
+import endreborn.init.EntitiesInit;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientProxy extends CommonProxy
 {
@@ -11,4 +14,9 @@ public class ClientProxy extends CommonProxy
 	{
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
 	}
+	@SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event) {
+;
+    	EntitiesInit.initModels();
+    }
 }
--- src/main/java/endreborn/proxy/CommonProxy.java
+++ src/main/java/endreborn/proxy/CommonProxy.java
@@ -1,9 +1,17 @@
 package endreborn.proxy;
 
+import endreborn.util.handlers.EventsHandler;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy 
 {
+    public void preInit(FMLPreInitializationEvent event)
+    {
+		MinecraftForge.EVENT_BUS.register(new EventsHandler());
+    }
 	public void registerItemRenderer(Item item, int meta, String id) {}	
-	
+	public void registerVariantRenderer(Item item, int meta, String filename, String id) {}
 }
+
